abecce2a2da68133cde0319457a919b4cbd79e4c
abdulwahhab2211996@gmail.com <abdulwahhab2211996@gmail.com>
2023-12-18 17:36:44 +0200

    add textarea

--- main.js
+++ main.js
@@ -36,3 +36,22 @@ function convertToCode() {
   // اعرض الناتج في عنصر p
   document.getElementById("output").innerHTML = output;
 }
+function copyText() {
+  // Get the div element
+  var divElement = document.getElementById("output");
+
+  // Create a range object
+  var range = document.createRange();
+
+  // Select the contents of the div element
+  range.selectNode(divElement);
+
+  // Add the range to the user's selection
+  window.getSelection().addRange(range);
+
+  // Copy the selected text to the clipboard
+  document.execCommand("copy");
+
+  // Give a visual feedback to the user that the text has been copied
+  alert("Text has been copied!");
+}
